579d4097645eb5a8da8215142ba62142442fdc93
nicolerv12 <niarova@gmail.com>
2024-11-07 10:11:05 -0300

    templates

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.product_list,name='product_list'),
-    path('created/', views.product_created, name='product_create'),
+    path('created/', views.product_created, name='product_created'),
     path('<int:id>/', views.product_detail,name='product_detail'),
     path('<int:id>/update/',views.product_update, name='product_update'),
     path('<int:id>/delete/',views.product_delete, name='product_delete'),
